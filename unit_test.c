bf502753296b6754193a4820a1076d521e12b2b3
Yu-Chih (Joyce) Cho <aejh82081@gmail.com>
2018-04-29 20:26:15 -0600

    Update unit_test.c

--- unit_test.c
+++ unit_test.c
@@ -6,10 +6,17 @@
 #include <cmocka.h>
 
 #include "UART.h"
+#include "socket_server.h"
 
 void test_uart_config(void **state)
 {
   assert_int_equal(0, uart4_init());
+}
+
+void test_socket_config(void **state)
+{
+  int status = socket_start();
+  assert_int_equal(0, status);
 }
 
 int main(int argc, char **argv)
@@ -17,6 +24,7 @@ int main(int argc, char **argv)
   const UnitTest tests []=
   {
     unit_test(test_uart_config),
+    unit_test(test_socket_config),
   };
   return run_tests(tests);
 }
